459996674d95bcf77a532e7b1a9d1193c0150255
robertpanvip <robertpanvip@163.com>
2026-01-28 23:08:48 +0800

    feat:完善 key

--- src/main/kotlin/com/pan/json2typescript/util/TsKey.kt
+++ src/main/kotlin/com/pan/json2typescript/util/TsKey.kt
@@ -1,15 +1,10 @@
 package com.pan.json2typescript.util
 
-private val TS_KEYWORD = setOf(
-)
-
 object TsKeyUtils {
 
     fun toTsKey(key: String): String {
         // 合法标识符 + 不是关键字
-        if (key.matches(Regex("""^[a-zA-Z_$][\w$]*$""")) &&
-            key !in TS_KEYWORD
-        ) {
+        if (key.matches(Regex("""^[a-zA-Z_$][\w$]*$"""))) {
             return key
         }
 

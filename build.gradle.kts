dffe2767e7e5ee3a3720d351e15ddd74034e8318
robertpanvip <robertpanvip@users.noreply.github.com>
2026-08-10 06:14:23 +0000

    chore: 配置 Gradle HTTP 代理 & 恢复 Java 21 兼容
    gradle.properties 添加 HTTP/HTTPS 代理配置以加速依赖下载。
    build.gradle.kts 恢复 JVM target 为 21。

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,11 +49,11 @@ intellijPlatform {
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        kotlinOptions.jvmTarget = "21"
     }
     test {
         useJUnitPlatform()
